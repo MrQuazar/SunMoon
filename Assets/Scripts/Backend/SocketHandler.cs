400baa843601e0dd4798e6dad3489dfbeeb08f5c
Abbas Kagalwala <abbaskagalwala15@gmail.com>
2026-02-19 12:02:05 +0000

    - Lobby created

--- Assets/Scripts/Backend/SocketHandler.cs
+++ Assets/Scripts/Backend/SocketHandler.cs
@@ -8,37 +8,39 @@ using System.Collections;
 
 public class SocketHandler : MonoBehaviour
 {
+    public static SocketHandler instance;
+
     private WebSocket ws;
 
     // URL to your WebSocket server
-    private string serverUrl = "ws://localhost:3000/ws"; // Change this to your server URL
+    private string serverUrl = "ws://10.104.77.29:3000/ws"; // Change this to your server URL
 
     [SerializeField] private Transform cube1;
     [SerializeField] private Transform cube2;
 
-    // Define a simple class for JSON serialization
-    [System.Serializable]
-    public class WebSocketMessage
+    private string[] players;
+
+    private string myID;
+
+    private void Awake()
     {
-        public string type;
-        public string roomId;
-        public string data;
-        public string did;
-        public Vector3 pos; // Example additional field for position data
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    [Serializable]
-    public class CreateRoomResponse
-    {
-        public string roomId;
-        public string message;
-    }
 
     [ContextMenu("Create New Room")]
-    private void CreateNewRoomRequest()
+    internal void CreateNewRoomRequest()
     {
         // 1. Create request with a callback
-        var request = HTTPRequest.CreateGet("http://localhost:3000/room/create",
+        var request = HTTPRequest.CreateGet("http://10.104.77.29:3000/room/create",
                                              CreateNewRoomResponse);
 
         // 3. Send request
@@ -82,6 +84,12 @@ public class SocketHandler : MonoBehaviour
         }
     }
 
+    internal void JoinRoomRequest(string roomId)
+    {
+        roomID = roomId;
+        JoinRoomWS();
+    }
+
     private void JoinRoomWS()
     {
         // Create WebSocket connection
@@ -92,7 +100,6 @@ public class SocketHandler : MonoBehaviour
         {
             Debug.Log("Connected to server");
             // After connection, join a room
-            JoinRoom(roomID);
         };
 
         ws.OnMessage += (WebSocket w, string message) =>
@@ -122,7 +129,7 @@ public class SocketHandler : MonoBehaviour
         {
             type = "join_room",
             roomId = roomId,
-            did = SystemInfo.deviceUniqueIdentifier,
+            // did = SystemInfo.deviceUniqueIdentifier,
             data = null // Optional field, can be used if needed
         };
 
@@ -172,6 +179,13 @@ public class SocketHandler : MonoBehaviour
         WebSocketMessage receivedMessage = JsonUtility.FromJson<WebSocketMessage>(message);
         string type = receivedMessage.type;
 
+        if (type == "connected")
+        {
+            Debug.Log("Received connected message. My ID: " + receivedMessage.clientId);
+            myID = receivedMessage.clientId;
+            JoinRoom(roomID);
+        }
+
         if (type == "receive_data")
         {
             // Handle received data from the other player
@@ -188,6 +202,14 @@ public class SocketHandler : MonoBehaviour
         {
             Debug.Log("Joined room successfully: " + receivedMessage.roomId);
             roomID = receivedMessage.roomId;
+            players = receivedMessage.players;
+            if (LobbySelectionScreen.instance != null)
+            {
+                if (LobbySelectionScreen.instance.gameObject.activeInHierarchy)
+                    LobbySelectionScreen.instance.OnJoinRoomSuccess(roomID, players.Length);
+                else if (LobbySelectionScreen.instance.lobbyWaitingScreen.gameObject.activeInHierarchy)
+                    LobbySelectionScreen.instance.lobbyWaitingScreen.SetRoomCode(roomID, players.Length);
+            }
         }
         else if (type == "peer_joined")
         {
@@ -228,168 +250,24 @@ public class SocketHandler : MonoBehaviour
             ws = null;
         }
     }
-    // [Header("Server Configuration")]
-    // [SerializeField] private string serverUrl = "ws://YOUR_SERVER_IP:3000/ws";  // WebSocket URL (your Node.js server)
-    // [SerializeField] private string roomId = "ABCD";  // Room ID to join
 
-    // private WebSocket websocket;
-    // private string myClientId;
+}
 
-    // // Start is called before the first frame update
-    // void Start()
-    // {
-    //     ConnectToServer();
-    // }
+// Define a simple class for JSON serialization
+[System.Serializable]
+public class WebSocketMessage
+{
+    public string type;
+    public string roomId;
+    public string data;
+    public string clientId;
+    public Vector3 pos; // Example additional field for position data
+    public string[] players; // Example additional field for player list
+}
 
-    // // Connect to the WebSocket signaling server
-    // private void ConnectToServer()
-    // {
-    //     websocket = new WebSocket(new Uri(serverUrl));
-
-    //     // When the connection is opened
-    //     websocket.OnOpen += OnOpen;
-    //     websocket.OnMessage += OnMessage;
-    //     // websocket.OnError += OnError;
-    //     // websocket.OnClosed += OnClosed;
-
-    //     // Connect
-    //     websocket.Open();
-    // }
-
-    // // Called when WebSocket connection is open
-    // private void OnOpen(object sender)
-    // {
-    //     Debug.Log("Connected to WebSocket server.");
-
-    //     // Join the room
-    //     JoinRoom(roomId);
-    // }
-
-    // // Send message to the server (WebSocket)
-    // private void SendMessageToServer(string messageType, string jsonData)
-    // {
-    //     var message = new
-    //     {
-    //         type = messageType,
-    //         data = jsonData
-    //     };
-    //     websocket.Send(JsonUtility.ToJson(message));
-    // }
-
-    // // Join room method
-    // private void JoinRoom(string roomId)
-    // {
-    //     var message = new { type = "join_room", roomId };
-    //     SendMessageToServer("join_room", JsonUtility.ToJson(message));
-    // }
-
-    // // Handle incoming messages
-    // private void OnMessage(object sender, string e)
-    // {
-    //     var message = e;
-    //     Debug.Log("Received: " + message);
-
-    //     // Parse the message
-    //     // var response = JsonUtility.FromJson<MessageResponse>(message);
-
-    //     // switch (response.type)
-    //     // {
-    //     //     case "room_created":
-    //     //         Debug.Log("Room created, my Client ID: " + response.data.clientId);
-    //     //         myClientId = response.data.clientId;
-    //     //         break;
-
-    //     //     case "room_joined":
-    //     //         Debug.Log("Joined room, my Client ID: " + response.data.clientId);
-    //     //         myClientId = response.data.clientId;
-    //     //         break;
-
-    //     //     case "room_full":
-    //     //         Debug.Log("Room is full, cannot join.");
-    //     //         break;
-
-    //     //     case "peer_joined":
-    //     //         Debug.Log("Peer joined: " + response.data.clientId);
-    //     //         break;
-
-    //     //     case "ready":
-    //     //         Debug.Log("Both peers are ready, start WebRTC negotiation.");
-    //     //         break;
-
-    //     //     case "signal":
-    //     //         HandleSignaling(response.data);
-    //     //         break;
-
-    //     //     case "peer_left":
-    //     //         Debug.Log("Peer left: " + response.data.clientId);
-    //     //         break;
-
-    //     //     default:
-    //     //         Debug.Log("Unhandled message type: " + response.type);
-    //     //         break;
-    //     // }
-    // }
-
-    // // Handle signaling (offer/answer/ice candidates)
-    // private void HandleSignaling(dynamic signalData)
-    // {
-    //     // Debug.Log("Received signal from " + signalData.from);
-
-    //     // switch (signalData.data.type)
-    //     // {
-    //     //     case "offer":
-    //     //         Debug.Log("Received offer, handle it");
-    //     //         // Handle offer (you can trigger the WebRTC logic here)
-    //     //         break;
-
-    //     //     case "answer":
-    //     //         Debug.Log("Received answer, handle it");
-    //     //         // Handle answer (you can trigger the WebRTC logic here)
-    //     //         break;
-
-    //     //     case "ice-candidate":
-    //     //         Debug.Log("Received ICE candidate, handle it");
-    //     //         // Handle ICE candidate (you can trigger the WebRTC logic here)
-    //     //         break;
-
-    //     //     default:
-    //     //         Debug.Log("Unknown signal type");
-    //     //         break;
-    //     // }
-    // }
-
-    // // Handle errors
-    // // private void OnError(object sender, BestHTTP.WebSocket.Events.ErrorEventArgs e)
-    // // {
-    // //     Debug.LogError("WebSocket Error: " + e.Exception);
-    // // }
-
-    // // // Handle WebSocket closure
-    // // private void OnClosed(object sender, BestHTTP.WebSocket.Events.ClosedEventArgs e)
-    // // {
-    // //     Debug.Log("WebSocket connection closed.");
-    // // }
-
-    // // // Close the connection
-    // // private void CloseConnection()
-    // // {
-    // //     if (websocket != null && websocket.State == WebSocketState.Open)
-    // //     {
-    // //         websocket.Close();
-    // //     }
-    // // }
-
-    // // Struct for response data
-    // [Serializable]
-    // private class MessageResponse
-    // {
-    //     public string type;
-    //     public dynamic data;
-    // }
-
-    // // For cleanup on closing
-    // // private void OnApplicationQuit()
-    // // {
-    // //     CloseConnection();
-    // // }
+[Serializable]
+public class CreateRoomResponse
+{
+    public string roomId;
+    public string message;
 }
