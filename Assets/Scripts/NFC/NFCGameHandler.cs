45885cf83b1baea54f7dc70d07962d8fb55a8367
Abbas Kagalwala <abbaskagalwala15@gmail.com>
2026-02-20 01:41:56 +0000

    - NFCGameHandler added

--- Assets/Scripts/NFC/NFCGameHandler.cs
+++ Assets/Scripts/NFC/NFCGameHandler.cs
@@ -5,20 +5,57 @@ namespace DigitsNFCToolkit.Samples
 {
     public class NFCGameHandler : MonoBehaviour
     {
+        public static NFCGameHandler instance;
         private NDEFMessage pendingMessage;
+
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
 
         #region Read
         public void OnMakeReadonlyClick()
         {
 #if (!UNITY_EDITOR)
+// Register listener to detect NFC tag info
+NativeNFCManager.AddNFCTagDetectedListener(OnNFCTagDetected);
+
+// Register listener to handle NDEF message read result
+NativeNFCManager.AddNDEFReadFinishedListener(OnNDEFReadFinished);
 			NativeNFCManager.Enable();
 			NativeNFCManager.RequestNDEFMakeReadonly();
 #endif
         }
 
+        void OnNFCTagDetected(NFCTag tag)
+        {
+            // Access tag information
+            Debug.Log("Tag ID: " + tag.ID);
+            Debug.Log("Technologies: " + string.Join(",", tag.Technologies));
+            Debug.Log("Writable: " + tag.Writable);
+        }
+
         public void OnNDEFReadFinished(NDEFReadResult result)
         {
             string readResultString = string.Empty;
+            //Get The TextRecord
+            result.Message.Records.ForEach(record =>
+            {
+                if (record is TextRecord)
+                {
+                    TextRecord textRecord = record as TextRecord;
+                    readResultString += string.Format("Text Record: {0}\n", textRecord.text);
+                    Debug.LogError(readResultString);
+                }
+            });
             if (result.Success)
             {
                 readResultString = string.Format("NDEF Message was read successfully from tag {0}", result.TagID);
@@ -49,10 +86,14 @@ namespace DigitsNFCToolkit.Samples
             Debug.Log(pushResultString);
         }
 
-        public void OnPushMessageClick()
+        public void OnPushMessageClick(string message)
         {
+            pendingMessage = new NDEFMessage();
+            pendingMessage.Records.Add(new TextRecord(message));
             if (pendingMessage != null)
             {
+                Debug.LogError(pendingMessage);
+
 #if (!UNITY_EDITOR) && UNITY_ANDROID
 				NativeNFCManager.RequestNDEFPush(pendingMessage);
 #endif
