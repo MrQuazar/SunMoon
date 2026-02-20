962b32650b9f19e17ac2f87a5b4c01700546199e
RaffLLE <raffyevangelista24@gmail.com>
2026-02-20 07:48:18 +0000

    starting UI handling

---
+++ Assets/MenuHandling/MenuHandler.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class MenuHandler : MonoBehaviour
+{
+    public Screens previousScreen;
+    public Screens currentScreen;
+
+    [Header("All Screens")]
+    public Screens settings;
+    public Screens countdown;
+    public Screens gameScreen;
+    public Screens hostGame;
+    public Screens joinGame;
+    public Screens mainMenu;
+    public Screens tutorial;
+    public Screens lose;
+    public Screens win;
+
+    [Header("Screen Handling")]
+    public Screens startScreen;
+
+    void Start()
+    {
+        // Set menu handler for all
+        settings.menuHandler = this;
+        // countdown.menuHandler = this;
+        // gameScreen.menuHandler = this;
+        hostGame.menuHandler = this;
+        joinGame.menuHandler = this;
+        mainMenu.menuHandler = this;
+        // tutorial.menuHandler = this;
+        // lose.menuHandler = this;
+        // win.menuHandler = this;
+
+        if (startScreen == null)
+        {
+            mainMenu.Enable();
+            currentScreen = mainMenu;
+        }
+        else
+        {
+            startScreen.Enable();
+            currentScreen = startScreen;
+        }
+    }
+
+    public void ChangeScreen(Screens nextScreen)
+    {
+        currentScreen.Disable();
+        previousScreen = currentScreen;
+        currentScreen = nextScreen;
+        currentScreen.Enable();
+    }
+
+    public void GoBack()
+    {
+        ChangeScreen(previousScreen);
+    }
+}
---
+++ Assets/MenuHandling/SettingsMenu.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenu : Screens
+{
+    public Button back;
+
+    internal override void AddListeners()
+    {
+        back.onClick.AddListener(OnBackPress);
+    }
+
+    private void OnBackPress()
+    {
+        menuHandler.GoBack();
+    }
+}
